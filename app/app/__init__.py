cd59d15b5aed4a18c7659c2992bd11e749cd6efd
GaZ33 <gasinho.ga@gmail.com>
2026-02-09 23:40:23 -0300

    Add initial project structure with main application files and .gitignore

---
+++ app/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# Todo: adicionar outras extensões conforme a implementação do projeto
+# Bcrypt para hashing de senhas, Flask-Login para gerenciamento de sessões, etc.
+# dotenv para carregar variáveis de ambiente, etc.
+# flask_login para gerenciamento de sessões, etc.
+# flask_migrate para migrações de banco de dados, etc.
+
+
+app = Flask(__name__)
